8ae3daf857b1b0a3476a4eec9116cd60d6b2e641
Kevin Clark <kevinwilliamclark@gmail.com>
2021-06-13 09:32:40 -0500

    Set up firebase config

---
+++ fire.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyACl4foIzeipOLWAK_zP2VnWy9luCUBzeY",
+  authDomain: "weather-app-4a21a.firebaseapp.com",
+  projectId: "weather-app-4a21a",
+  storageBucket: "weather-app-4a21a.appspot.com",
+  messagingSenderId: "995730832541",
+  appId: "1:995730832541:web:514205d23c55806c07a874",
+  measurementId: "G-1GNV9T4ECV"
+};
